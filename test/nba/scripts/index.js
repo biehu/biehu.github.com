276e3b37c51d47d4e2046f206f6eb7330522bb1e
biehu <webqianduan@gmail.com>
2015-01-31 18:47:33 +0800

    test

--- test/nba/scripts/index.js
+++ test/nba/scripts/index.js
@@ -161,18 +161,124 @@ var load = function () {
  */
 var cup = function () {
     var cupImg = new Image();
-    var canvas = $('#cup')[0];
+    var canvas = document.getElementById('cup');
     var context = canvas.getContext('2d');
-    
-    alert(0);
+    var winWidth = $(window).width();
+    var imgHeight, imgWidth;
     
     var drawImg = function () {
-        alert(1);
-        context.draeImage(canvas, 0, 0);
+        var w = cupImg.width;
+        var h = cupImg.height;
+       
+        imgWidth = winWidth;
+        imgHeight = winWidth * h / w;
+        
+        canvas.width = winWidth;
+        canvas.height = imgHeight;
+        context.drawImage(cupImg, 0, 0, imgWidth, imgHeight);
     };
     
     cupImg.onload = drawImg;
-    cupImg.src = '../images/cup.jpg';
+    cupImg.src = 'http://www.biehu.me/test/nba/images/p1.jpg';
+};
+
+/*
+ * 生成图片
+ */
+var drawCanvas = function () {
+    var canvas, board, img;
+    canvas = document.getElementById('cup');
+    img = document.getElementById('img');
+    
+    canvas.height = 200;
+    canvas.width = 300;
+    
+    board = canvas.getContext('2d');
+    
+    var mousePress = false;
+    var last = null;
+    
+    function beginDraw(){
+        mousePress = true;
+    }
+    
+    function drawing(event){
+        event.preventDefault();
+        if (!mousePress) 
+            return;
+        var xy = pos(event);
+        if (last != null) {
+            board.beginPath();
+            board.moveTo(last.x, last.y);
+            board.lineTo(xy.x, xy.y);
+            board.stroke();
+        }
+        last = xy;
+        
+    }
+    
+    function endDraw(event){
+        mousePress = false;
+        event.preventDefault();
+        last = null;
+    }
+    
+    function pos(event){
+        var x, y;
+        if (isTouch(event)) {
+            x = event.touches[0].pageX;
+            y = event.touches[0].pageY;
+        }
+        else {
+            x = event.offsetX + event.target.offsetLeft;
+            y = event.offsetY + event.target.offsetTop;
+        }
+        //               log('x='+x+' y='+y);
+        return {
+            x: x,
+            y: y
+        };
+    }
+    
+    function log(msg){
+        var log = document.getElementById('log');
+        var val = log.value;
+        log.value = msg + '\n' + val;
+    }
+    
+    function isTouch(event){
+        var type = event.type;
+        if (type.indexOf('touch') >= 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    function save(){
+        //base64
+        var dataUrl = canvas.toDataURL();
+        //                dataUrl = dataUrl.replace("image/png", "image/octet-stream");
+        img.src = dataUrl;
+    }
+    
+    
+    function clean(){
+        board.clearRect(0, 0, canvas.width, canvas.height);
+        
+    }
+    
+    board.lineWidth = 2;
+    board.strokeStyle = "#0000ff";
+    
+    
+    canvas.onmousedown = beginDraw;
+    canvas.onmousemove = drawing;
+    canvas.onmouseup = endDraw;
+    canvas.addEventListener('touchstart', beginDraw, false);
+    canvas.addEventListener('touchmove', drawing, false);
+    canvas.addEventListener('touchend', endDraw, false);
 };
 
 $(function () {
@@ -181,5 +287,7 @@ $(function () {
 //   page();
 //   load();
 cup();
+
+drawCanvas();
 })
 
