385a10394ea391beeca32a9aac58eaa913433a7d
biehu1 <276620879@qq.com>
2015-10-17 20:55:02 +0800

    yayang

---
+++ test/yayang/scripts/index.js
@@ -0,0 +1,107 @@
+var tab = function () {
+    var root = $('.tab');
+    
+    var changeTab = function () {
+        var link = $(this);
+        var num = link.attr('data-num') - 1;
+        root.find('.tab-title p').removeClass('on').
+            eq(num).addClass('on');
+        root.find('.tab-con p').removeClass('on').
+            eq(num).addClass('on');
+    };
+    
+    $('.link').click(changeTab);
+};
+
+
+tab();
+
+
+(function () {
+
+   /*
+    * 翻页
+    */
+   var yStart, yEnd;
+   var index = 0;
+   var allNum = $('.wrap').find('p').length;
+   var isAnimate = false;
+   var isModal = false;
+   
+   var slide = function (el, num) {
+        el.css('WebkitTransitionDuration', '.5s');
+        el.css('webkitTransform', 'translateY(' + num +　')');
+   };
+   
+   var setPage = function (dir,page) {
+       
+        isAnimate = true;
+        
+        if(dir === 1){
+            $(".p"+(page+1)).addClass('active').removeClass('up');
+            $(".p"+(page+2)).removeClass('active').addClass('down');
+            
+        }
+        else{
+            $(".p"+page).removeClass('active').addClass('up');
+            $(".p"+(page+1)).addClass('active').removeClass('down');
+        }
+       
+        // 翻页
+        setTimeout(function(){
+            if (isAnimate) {
+                isAnimate = false;
+            }
+        },750);
+    };
+    
+    var bindTouchPageUp = function(){
+    
+        $(document).bind("touchstart", function(e){
+            var touch = e.originalEvent.touches[0];
+            yStart = touch.pageY;
+        });
+        $(document).bind("touchmove", function(e){
+            event.preventDefault();
+            var touch = e.originalEvent.touches[0];
+            yEnd = touch.pageY;
+            if (isAnimate || isModal) 
+                return false;
+            if (yEnd - 100 > yStart & yEnd > yStart) { //down
+                if (index === 0)
+                    return false;
+                setPage(1, --index);
+            }
+            else 
+                if (yEnd + 100 < yStart & yEnd < yStart) { //up
+                    if (index === (allNum - 1)) 
+                        return false;
+                    setPage(0, ++index);
+                }
+        });
+        
+    };
+
+    
+   var bindPageUp = function () {
+       $(".next").bind('click', function(e){
+           e.preventDefault();
+           if (index == (allNum - 1)) {
+               return;
+           }
+           
+           if (isAnimate) {
+               isAnimate = false;
+           }
+           
+           setPage(0, ++index);
+       });
+   };
+
+   var init = function () {
+       bindPageUp();
+       bindTouchPageUp();
+   };
+
+   init();
+}());
