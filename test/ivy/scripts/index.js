9a1450eea39a5c0fd20968f7f738a609c6e02842
biehu1 <276620879@qq.com>
2015-10-31 00:30:09 +0800

    ivy

---
+++ test/ivy/scripts/index.js
@@ -0,0 +1,50 @@
+var focus = function () {
+    var wrap = $('.slide');
+    var items = wrap.find('.item');
+    var now = 0;
+    var interval;
+
+    var move = function () {
+        items.hide();
+        items.eq(now).fadeIn(1000);
+    };
+
+    var toLeft = function () {
+        now--;
+        if (now < 0) {
+            now = items.length - 1;
+        }
+        move();
+        return false;
+    };
+
+    var toRight = function () {
+        now++;
+        if (now > items.length - 1) {
+            now = 0;
+        }
+        move();
+        return false;
+    };
+    
+     var handleSlide = function () {
+         var slide = $(this).attr('data-slide');
+         clearInterval(interval);
+         if (slide === 'prev') {
+             toLeft();
+         }
+         else {
+             toRight();
+         }
+         interval = setInterval(function () {
+            toRight();
+         }, 3000);
+     };
+    
+    interval = setInterval(function () {
+        toRight();
+    }, 3000);
+
+    $('.slide .carousel-control.left, .slide .carousel-control.right').click(handleSlide);
+};
+focus();
