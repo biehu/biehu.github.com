901e2fd38959eda90fd2be46794d26bb734fcf10
biehu1 <276620879@qq.com>
2016-01-23 21:14:48 +0800

    test

--- test/pingtai/js/zoom_img.js
+++ test/pingtai/js/zoom_img.js
@@ -3,43 +3,55 @@
     
     var oShow = oDiv.getElementsByTagName('div')[0];
     var wrapPos = $(oShow).offset();
+    var wrapPosLeft = wrapPos.left;
+    var wrapPosTop = wrapPos.top;
     
     var oSpan = oDiv.getElementsByTagName('span')[0];
     var oImg = document.getElementById('img1');
+    var oImgWrap = oImg.parentNode;
     
-    oShow.onmouseover = function(){
-        oSpan.style.display = 'block';
-        oImg.parentNode.style.display = 'block';
-    };
-    oShow.onmouseout = function(){
-        oSpan.style.display = 'none';
-        oImg.parentNode.style.display = 'none';
-    };
-    oShow.onmousemove = function(ev){
+    var oShowWidth = $(oShow).width();
+    var oShowHeight = $(oShow).height();
+    var oSpanWidth = $(oSpan).width();
+    var oSpanHeight = $(oSpan).height();
+    
+    $(oShow).on('mouseenter', function(){
+        $(oSpan).show();
+        $(oImgWrap).show();
+    });
+    $(oShow).on('mouseleave', function(){
+        $(oSpan).hide();
+        $(oImgWrap).hide();
+    });
+    $(oShow).on('mousemove', function(ev){
         var oEvent = ev || event;
         
-        var x = oEvent.pageX - wrapPos.left - oSpan.offsetWidth / 2;
-        var y = oEvent.pageY - wrapPos.top - oSpan.offsetHeight / 2;
+        var mouseX = oEvent.pageX;
+        var mouseY = oEvent.pageY;
+        
+        var x = mouseX - wrapPosLeft - oSpanWidth / 2;
+        var y = mouseY - wrapPosTop - oSpanHeight / 2;
         
         if (x < 0) {
             x = 0;
+        } else if (x > oShowWidth - oSpanWidth) {
+            x = oShowWidth - oSpanWidth;
         }
-        else if (x > oShow.offsetWidth - oSpan.offsetWidth) {
-                x = oShow.offsetWidth - oSpan.offsetWidth;
-            }
         if (y < 0) {
             y = 0;
         }
-        else if (y > oShow.offsetHeight - oSpan.offsetHeight) {
-                y = oShow.offsetHeight - oSpan.offsetHeight
-            }
+        else if (y > oShowHeight - oSpanHeight) {
+            y = oShowHeight - oSpanHeight;
+        }
         
-        oSpan.style.left = x + 'px';
-        oSpan.style.top = y + 'px';
-        var percentX = x / (oShow.offsetWidth - oSpan.offsetWidth);
-        var percentY = y / (oShow.offsetHeight - oSpan.offsetHeight);
-        var oImgparent = oImg.parentNode;
-        oImg.style.left = -percentX * (oImg.offsetWidth - oImgparent.offsetWidth) + 'px';
-        oImg.style.top = -percentY * (oImg.offsetHeight - oImgparent.offsetHeight) + 'px';
-    };
+        $(oSpan).css({left: x + 'px', top: y + 'px'});
+        
+        var percentX = x / (oShowWidth - oSpanWidth);
+        var percentY = y / (oShowHeight - oSpanHeight);
+        
+        $(oImg).css({
+            left: -percentX * ($(oImg).width() - $(oImgWrap).width()) + 'px',
+            top: -percentY * ($(oImg).height() - $(oImgWrap).height()) + 'px'
+        });
+    });
 })();
