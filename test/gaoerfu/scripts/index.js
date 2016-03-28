f3b98646a726b283a77b9fddb5c45ed97ae3a6c1
biehu1 <276620879@qq.com>
2016-03-29 00:33:10 +0800

    test

---
+++ test/gaoerfu/scripts/index.js
@@ -0,0 +1,85 @@
+// 渐变焦点图
+var Focus = function (wrapId) {
+    if (typeof wrapId === 'String') {
+        this.wrap = $('#' + wrapId);
+    } else {
+        this.wrap = wrapId;
+    }
+    
+    this.items = this.wrap.find('.item');
+    this.pointLinks = this.wrap.find('.carousel-indicators li');
+    this.now = 0;
+    this.interval; 
+    
+    this.init();
+};
+Focus.prototype.move = function () {
+    this.pointLinks.removeClass('active');
+    this.pointLinks.eq(this.now).addClass('active');
+    
+    this.items.fadeOut();
+    this.items.eq(this.now).fadeIn(1000);
+};
+
+Focus.prototype.toLeft = function () {
+    this.now--;
+    if (this.now < 0) {
+        this.now = this.items.length - 1;
+    }
+    this.move();
+    return false;
+};
+
+Focus.prototype.toRight = function () {
+    this.now++;
+    if (this.now > this.items.length - 1) {
+        this.now = 0;
+    }
+    this.move();
+    return false;
+};
+
+ Focus.prototype.handleSlide = function (link) {
+     var _this = this;
+     var slide = $(link).attr('data-slide-to');
+     clearInterval(this.interval);
+     
+     this.now = slide;
+     this.move();
+     
+     this.interval = setInterval(
+     function () {
+        _this.toRight();
+     }, 3000);
+ };
+Focus.prototype.init = function () {
+    var _this = this;
+    this.interval = setInterval(
+    function () {
+       _this.toRight();
+    }, 
+    3000);
+
+    this.pointLinks.click(
+    function () {
+        _this.handleSlide(this);
+    });
+};
+
+
+new Focus($('#slide_1'));
+
+var tab = function () {
+    $('.tab_title a').click(function () {
+        var linkWrap = $(this).parent('li');
+		
+		linkWrap.addClass('on').siblings().removeClass('on');
+		
+		$('.tab').removeClass('on');
+		$('.tab_' + (linkWrap.index() + 1)).addClass('on');
+        
+		return false;
+    });
+};
+
+tab();
